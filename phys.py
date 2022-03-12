34cf02672c1b61a71de453aef70e10e9924c95ff
ezhang7423 <ezhang7423@student.palomar.edu>
2022-03-12 01:32:44 -0800

    addd all

---
+++ phys.py
@@ -0,0 +1,9 @@
+from math import *
+
+# electricity and magnetism
+k = 8.98e9
+eps_0 = 8.854e-12
+proton_charge = 1.602e-19
+proton_mass = 1.672e-27
+electron_charge = -proton_charge
+electron_mass = 9.109e-31
